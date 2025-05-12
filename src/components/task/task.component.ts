87eafb01c01768b8859070e90f1a501801070467
Federico Dubuch <fede25-5-1992@hotmail.com>
2025-05-13 00:25:13 +0200

    Modern Angular pipes and custom pipes deep dive.

--- src/components/task/task.component.ts
+++ src/components/task/task.component.ts
@@ -1,15 +1,16 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
 import { DatePipe } from "@angular/common";
 
 import { type Task } from "./task.model";
 import { TaskService } from '../../services/tasks.service';
 import { CardComponent } from "../../shared/card/card.component";
-import { SafeLinkDirective } from '../../directives/safe-link.directive'; // Atribute self directive
+import { TemperaturePipe } from '../../pipes/temperature.pipe'; // Custom Pipe for temperature
 import { AuthDirective } from '../../directives/auth.directive'; // Estructural self directive
+import { SafeLinkDirective } from '../../directives/safe-link.directive'; // Atribute self directive
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [CardComponent, DatePipe, SafeLinkDirective, AuthDirective],
+  imports: [CardComponent, DatePipe, TemperaturePipe, SafeLinkDirective, AuthDirective],
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss',
   hostDirectives: [] // Automatically applies directives from the array whenever the component is used.
@@ -17,6 +18,8 @@ import { AuthDirective } from '../../directives/auth.directive'; // Estructural 
 export class TaskComponent {
   task = input.required<Task>();
   taskService = inject(TaskService);
+
+  temperature = signal(39);
 
   onCompleteTask() {
     this.taskService.removeTask(this.task().id);
---
+++ src/pipes/temperature.pipe.ts
@@ -0,0 +1,33 @@
+import { Pipe, PipeTransform } from '@angular/core';
+@Pipe({
+  name: 'temperature',
+  standalone: true,
+  pure: true // When pure is false, turns off the default security chache mecanism, which prevents too many pipe executions.
+})
+export class TemperaturePipe implements PipeTransform {
+
+  transform(value: string | number | null, inputType: 'cel' | 'fah', outputType?: 'cel' | 'fah') {
+    let val: number;
+
+    if (!value) { return value };
+
+    if (typeof value === 'string') { val = parseFloat(value) } else { val = value };
+
+    let outputTemp: number;
+
+    if (inputType === 'cel' && outputType === 'fah') {
+      outputTemp = val * (9 / 5) + 32;
+    } else if (inputType === 'fah' && outputType === 'cel') {
+      outputTemp = (val - 32) * (5 / 9);
+    } else {
+      outputTemp = val;
+    }
+
+    let symbol: 'ºC' | 'ºF';
+
+    if (!outputType) { symbol = inputType === 'cel' ? 'ºC' : 'ºF' } else { symbol = outputType === 'cel' ? 'ºF' : 'ºC' };
+
+    return `${outputTemp.toFixed(2)} ${symbol}`;
+  }
+
+}
