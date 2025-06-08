28c6d5c3ffcbdce431b2fa1fd6e9443f7180eb9a
Federico Dubuch <fede25-5-1992@hotmail.com>
2025-06-08 11:44:40 +0200

    Using a11y to improve accessibility of templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { DUMMY_LESSONS } from "../assets/dummy-data"
-
-import { Component, inject } from '@angular/core';
+import { Component, inject, AfterViewInit, Inject } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
+import { filter } from 'rxjs/operators';
 import { HeaderComponent } from "../components/header/header.component";
 import { LessonComponent } from "../components/lesson/lesson.component";
-import { ErrorService } from '../shared/modal-error/service/error.service';
 import { ErrorModalComponent } from '../shared/modal-error/components/error/error-modal.component';
-import { RouterOutlet } from '@angular/router';
+import { ErrorService } from '../shared/modal-error/service/error.service';
+import { DUMMY_LESSONS } from "../assets/dummy-data"
 @Component({
   selector: 'app-root-angular-19',
   standalone: true,
@@ -14,11 +15,25 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
   hostDirectives: [] // Automatically applies directives from the array whenever the component is used.
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   lessons = DUMMY_LESSONS;
   selectedLessonId? = '';
   errorService = inject(ErrorService);
   error = this.errorService.error;
+
+  private router = inject(Router);
+  private document = inject(DOCUMENT);
+
+  ngAfterViewInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      const mainContent = this.document.getElementById('main-content');
+      if (mainContent) {
+        mainContent.focus();
+      }
+    });
+  }
 
   get selectedLesson(): { id: string; name: string; avatar: string } {
     return this.lessons.find((lesson) => lesson.id === this.selectedLessonId)!;
--- src/components/not-found/not-found.component.ts
+++ src/components/not-found/not-found.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
 @Component({
   selector: 'app-not-found',
   standalone: true,
@@ -8,6 +7,8 @@ import { RouterLink } from '@angular/router';
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements AfterViewInit {
+  @ViewChild('mainContent') mainContent!: ElementRef<HTMLElement>;
 
+  ngAfterViewInit() { this.mainContent.nativeElement.focus() };
 }
